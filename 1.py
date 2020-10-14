2c063ceb28d8cee0edb70b3de3601324991a3528
Настя <a.zemtseva@g.nsu.ru>
2020-10-14 09:29:46 +0600

    Done

--- 1.py
+++ 1.py
@@ -1,11 +1,17 @@
 class ToMass:
+
     def __init__(self, lst):
         self.lst = lst
+
     def __getitem__(self, item):
-        return self.lst[item-1]
-    def __setitem__(self, item,value):
-            self.lst[item-1] = value
+        return self.lst[item - 1]
+
+    def __setitem__(self, item, value):
+        self.lst[item - 1] = value
+
     def __str__(self):
         return self.lst
-a = ToMass([1,2,4])
+
+
+a = ToMass([1, 2, 4])
 print(a[1])
